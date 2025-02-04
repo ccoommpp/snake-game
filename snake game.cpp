97fd50b3ba45b53684f9820053c6d75c3183844a
chytanya <chytanyar@gmail.com>
2025-02-04 22:02:32 +0530

    layout and points are done

---
+++ snake game.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <chrono>
+#include <iostream>
+#include <cstdlib> 
+#include <ctime>
+using namespace std;
+void print_layout(string (&a)[50][50])
+{
+    for (int i = 0; i < 50; i++) {
+        for (int j = 0; j < 50; j++) {
+            cout << (a[i][j]);
+        }
+        cout << endl;
+    }
+}
+int random_x(int m)
+{
+    int x=(rand() %m)+1;
+    return x;
+}
+
+int random_y(int m)
+{
+    int y=(rand() %m)+1;
+    return y;
+}
+
+int main()
+{
+    srand(time(0));
+    int loss_check=1;
+    do
+    {
+    string a[50][50]{}; 
+
+    for (int i = 0; i < 50; i++)
+    {
+        a[0][i] = "*";
+    }
+
+
+    for (int i = 1; i < 49; i++)
+    {
+        a[i][0] = "*";
+        for(int j=1;j<49;j++)
+        {
+            a[i][j]=" ";
+        }     
+        a[i][49]="*";
+    }
+
+    for (int i = 0; i < 50; i++)
+    {
+        a[49][i] = "*";
+    }
+    int point_x=random_x(48);
+    int point_y=random_y(48);
+    a[point_x][point_y]="0";
+    print_layout(a);
+    }while(loss_check!=1);
+}
