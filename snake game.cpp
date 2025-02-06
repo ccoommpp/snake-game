7cea543bbe7da5970969bcf0b0db107e76ed1d91
ccoommpp <ccoommpp123@gmail.com>
2025-02-06 11:27:27 +0530

    basic are woring points and snake length to be added

--- snake game.cpp
+++ snake game.cpp
@@ -6,17 +6,57 @@
 
 using namespace std;
 
-char input() {
-    int timeLimit = 1000; 
-    int step = 50;      
-    int elapsedTime = 0;
-    char direction = 'w'; 
+void movements(string (&a)[10][20],char &A,bool &loss_check,int &x,int &y)
+{
+    if (a[x][y]=="H") 
+    {  
+    a[x][y] =" ";  // Replace 'H' with a space
+    }
+    else if(a[x][y]=="*")
+    {
+        loss_check=true;
+        return;
+    }
+    if(A=='w')
+    {
+        x--;
+    }
+    else if(A=='s')
+    {
+        x++;
+    }
+    else if(A=='a')
+    {
+        y--;
+    }
+    else if(A=='d')
+    {
+        y++;
+    }
 
-    while (elapsedTime < timeLimit) {
-        if (_kbhit()) {  
+    if(a[x][y]=="*")
+    {
+        loss_check=true;
+        return;
+    }
+    a[x][y]="H";
+}
+
+char input(char &direction) 
+{
+    int timeLimit = 1000; 
+    int step = 100;      
+    int elapsedTime = 0;
+    cout<<"\n enter now \n";
+    while (elapsedTime < timeLimit) 
+    {
+        if (_kbhit()) 
+        {  
             char ch = _getch(); 
-            if (ch == 'w' || ch == 'a' || ch == 's' || ch == 'd') {
-                return ch; 
+            if (ch == 'w' || ch == 'a' || ch == 's' || ch == 'd') 
+            {
+                direction=ch;
+                return direction; 
             }
         }
         Sleep(step);
@@ -48,12 +88,12 @@ void print(string (&a)[10][20]) {
 }
 
 int main() {
+    char direction='w';
     srand(time(0));
     int fruit_count = 0;
     string a[10][20]{};
     bool loss_check = false;
-
-     for (int i = 0; i < 20; i++)
+    for (int i = 0; i < 20; i++)
     {
         a[0][i] = "*";
     }
@@ -70,19 +110,18 @@ int main() {
     {
         a[9][i] = "*";
     }
-
+    int x=5;
+    int y=10;
+    a[x][y]="H";
     while(!loss_check)
     {
-        fruits(a, fruit_count);
+        /*fruits(a, fruit_count);*/
         print(a);
         Sleep(2000);
-        char a=input();
-        cout<<a;
-        Sleep(5000);
-        /*movement();
-        bool loss_check=loss_chq(a);
-        system("cls");*/
-
+        direction=input(direction);
+        Sleep(2000);
+        movements(a,direction,loss_check,x,y);
+        system("cls");
     }
 }
 
