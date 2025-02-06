e184cbe3a518e4b28a889199a05dfb21ae121061
ccoommpp <ccoommpp123@gmail.com>
2025-02-06 10:26:05 +0530

    its working lets goooo

--- snake game.cpp
+++ snake game.cpp
@@ -1,63 +1,64 @@
 #include <iostream>
 #include <cstdlib> 
 #include <ctime>
-#include<windows.h>
+#include <windows.h>
+#include <conio.h>
+
 using namespace std;
-void print_layout(string (&a)[50][50])
+
+void fruits(string (&a)[10][20], int &fruit_count)
 {
-    for (int i = 0; i < 50; i++) {
-        for (int j = 0; j < 50; j++) {
-            cout << (a[i][j]);
+    int x = rand() % 7+1;
+    int y = rand() % 17+1; 
+
+    if (fruit_count == 0)
+    {
+        a[x][y] = "0";
+        fruit_count++;
+    }
+}
+
+void print(string (&a)[10][20]) {
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 20; j++)
+        {
+            cout << a[i][j];
         }
         cout << endl;
     }
 }
-int random_x(int m)
-{
-    int x=(rand() %m)+1;
-    return x;
-}
 
-int random_y(int m)
-{
-    int y=(rand() %m)+1;
-    return y;
-}
-
-int main()
-{
+int main() {
     srand(time(0));
-    int loss_check=1;
-    do
-    {
-    string a[50][50]{}; 
+    int fruit_count = 0;
+    string a[10][20]{};
+    bool loss_check = false;
 
-    for (int i = 0; i < 50; i++)
+     for (int i = 0; i < 20; i++)
     {
         a[0][i] = "*";
     }
-
-
-    for (int i = 1; i < 49; i++)
+    for (int i = 1; i < 9; i++)
     {
         a[i][0] = "*";
-        for(int j=1;j<49;j++)
+        for(int j=1;j<19;j++)
         {
             a[i][j]=" ";
         }     
-        a[i][49]="*";
+        a[i][19]="*";
+    }
+    for (int i = 0; i < 20; i++)
+    {
+        a[9][i] = "*";
     }
 
-    for (int i = 0; i < 50; i++)
+    while(!loss_check)
     {
-        a[49][i] = "*";
+        fruits(a, fruit_count);
+        print(a);
+        Sleep(2000);
+        system("cls");
     }
-    int point_x=random_x(48);
-    int point_y=random_y(48);
-    a[point_x][point_y]="0";
-    Sleep(2000);
-    print_layout(a);
-    loss_check++;
-    }while(loss_check!=10);
-    return 0;
 }
+
